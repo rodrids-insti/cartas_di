6dbfaab538d96c6d609385a7b50e22d0e4326989
rodrids-insti <roddavsan@alu.edu.gva.es>
2024-10-27 02:01:33 +0200

    Proyecto con librerías importadas

---
+++ CartaBaraja/CartaBaraja/Card.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartaBaraja
+{
+    public enum Palos
+    {
+        DIAMANTES = '♦',
+        PICAS = '♠',
+        TREBOLES = '♣',
+        CORAZONES = '♥'
+    }
+    public enum Valores
+    {
+        AS = 1,
+        DOS,
+        TRES,
+        CUATRO,
+        CINCO,
+        SEIS,
+        SIETE,
+        OCHO,
+        NUEVE,
+        DIEZ,
+        JOTA,
+        REINA,
+        REY
+    }
+
+    public class Card
+    {
+        private Palos palo;
+        private Valores valor;
+
+        public Card(Palos palo, Valores valor)
+        {
+            this.palo = palo;
+            this.valor = valor;
+        }
+
+        public Palos getPalo()
+        {
+            return palo;
+        }
+        public Valores getValor()
+        {
+            return valor;
+        }
+
+        public String showCard()
+        {
+            String mostrar_valor = "";
+            switch (this.valor)
+            {
+                case Valores.AS:
+                    mostrar_valor = " A";
+                    break;
+                case Valores.JOTA:
+                    mostrar_valor = " J";
+                    break;
+                case Valores.REINA:
+                    mostrar_valor = " Q";
+                    break;
+                case Valores.REY:
+                    mostrar_valor = " K";
+                    break;
+                case Valores.DIEZ:
+                    mostrar_valor = "10";
+                    break;
+                default:
+                    mostrar_valor = " " + ((int)this.valor).ToString();
+                    break;
+            }
+            String result = "┌───────┐\n|       |\n|       |\n|  " + (char) this.palo + mostrar_valor + "  |\n|       |\n|       |\n└───────┘";
+            return result;
+        }
+    }
+}
---
+++ CartaBaraja/CartaBaraja/Deck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartaBaraja
+{
+    public class Deck
+    {
+        private const int size = 52;
+        private List<Card> cards = new List<Card>(size);
+        private int pos_actual = 0;
+
+        public Deck()
+        {
+            foreach (Palos palo in Enum.GetValues(typeof(Palos)))
+            {
+                foreach (Valores valor in Enum.GetValues(typeof(Valores)))
+                {
+                    cards.Add(new Card(palo, valor));
+                }
+            }
+        }
+
+        public Card nextCard()
+        {
+            return cards[pos_actual++];
+        }
+        public void sacarCarta()
+        {
+            Console.WriteLine(cards[pos_actual].showCard());
+        }
+        public void barajar()
+        {
+            Random n = new Random();
+
+            for (int i = cards.Count - 1; i > 0; i--)
+            {
+                int pos = n.Next(0, i + 1);
+                Card temp = cards[i];
+                cards[i] = cards[pos];
+                cards[pos] = temp;
+            }
+        }
+
+        public void showDeck()
+        {
+            foreach(Card card in cards)
+            {
+                Console.WriteLine(card.showCard());
+            }
+        }
+    }
+}
